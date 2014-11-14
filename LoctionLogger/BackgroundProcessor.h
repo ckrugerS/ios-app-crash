e350191a53053288311b2cdf3f806e966125f57a
ckrugerS <Kernel13>
2014-11-14 12:04:42 +0200

    first commit

---
+++ LoctionLogger/BackgroundProcessor.h
@@ -0,0 +1,13 @@
+//
+//  BackgroundProcessor.h
+//  LoctionLogger
+//
+//  Created by Cornelis Kruger on 2014/11/14.
+//  Copyright (c) 2014 private. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BackgroundProcessor : NSObject
+
+@end
---
+++ LoctionLogger/BackgroundProcessor.m
@@ -0,0 +1,13 @@
+//
+//  BackgroundProcessor.m
+//  LoctionLogger
+//
+//  Created by Cornelis Kruger on 2014/11/14.
+//  Copyright (c) 2014 private. All rights reserved.
+//
+
+#import "BackgroundProcessor.h"
+
+@implementation BackgroundProcessor
+
+@end
